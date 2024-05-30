66ba8f0239aed19dc42d01a838907880e65eb273
Ester <ester@linumlabs.com>
2024-05-30 18:39:23 +0200

    used connection string

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,21 +1,6 @@
 import { defineConfig } from "drizzle-kit";
-import {
-  DB_HOST,
-  DB_PORT,
-  DB_USER,
-  DB_PASSWORD,
-  DB_NAME,
-} from "./src/config/variables";
+ß;
 
 export default defineConfig({
-  dialect: "postgresql",
-  schema: "./src/db/schema.ts",
-  out: "./src/db/migrations",
-  dbCredentials: {
-    host: DB_HOST,
-    port: DB_PORT,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-  },
+  url: "postgres://postgres:12345@127.0.0.1:5432/be-intro",
 });
